b0eee4e4a1134115dbb4c7724e819ad87fd90237
Maria <tsvetkovamariia@gmail.com>
2016-09-10 13:51:28 +0600

    added option to delete photo for correct user

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 class PhotosController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :correct_user, only: :destroy 
 
   def index
     @photos = Photo.all
@@ -24,6 +25,9 @@ class PhotosController < ApplicationController
   end
 
   def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
+    redirect_to root_path
   end
 
   private
@@ -32,4 +36,9 @@ class PhotosController < ApplicationController
     params.require(:photo).permit(:name, :image, :user_id)
   end
   
+  def correct_user
+    @photo = Photo.find(params[:id])
+    redirect_to(root_path) unless current_user == @photo.user
+  end
+
 end
