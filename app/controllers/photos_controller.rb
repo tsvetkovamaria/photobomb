5a86f27a4e5d680ebadda3bdedcde17164cc2b6c
Maria <tsvetkovamariia@gmail.com>
2016-09-10 13:35:55 +0600

    Added image upload to Product model

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,16 +1,26 @@
 class PhotosController < ApplicationController
+
   before_action :authenticate_user!
 
   def index
+    @photos = Photo.all
   end
 
   def show
+    @photo = Photo.find(params[:id])
   end
 
   def new
+    @photo = Photo.new
   end
 
   def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      redirect_to @photo
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -18,5 +28,8 @@ class PhotosController < ApplicationController
 
   private
 
+  def photo_params
+    params.require(:photo).permit(:name, :image)
+  end
   
 end
